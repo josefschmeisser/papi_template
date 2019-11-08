eb8e4fda41b4966b1a2923ddf9a3cfe5d4131d48
Josef Schmeißer <josef.schmeisser@tum.de>
2019-11-08 16:03:58 +0100

    Initial commit

---
+++ lib/papi.cmake
@@ -0,0 +1,30 @@
+include(ExternalProject)
+find_package(Git REQUIRED)
+
+set(PAPI_PREFIX "papi")
+
+ExternalProject_Add(
+    papi_src
+    PREFIX ${PAPI_PREFIX}
+    GIT_REPOSITORY "https://bitbucket.org/icl/papi.git"
+    GIT_TAG "stable-5.7"
+    TIMEOUT 10
+    SOURCE_SUBDIR "src"
+    BUILD_IN_SOURCE True
+    CONFIGURE_COMMAND ./configure --prefix=<INSTALL_DIR>
+    BUILD_COMMAND     ${CMAKE_MAKE_PROGRAM}
+    INSTALL_COMMAND   ${CMAKE_MAKE_PROGRAM} install
+    UPDATE_COMMAND ""
+)
+
+# Prepare PAPI
+ExternalProject_Get_Property(papi_src install_dir)
+set(PAPI_INCLUDE_DIR ${install_dir}/include)
+set(PAPI_LIBRARY_PATH ${install_dir}/lib/libpapi.a)
+file(MAKE_DIRECTORY ${PAPI_INCLUDE_DIR})
+add_library(papi STATIC IMPORTED)
+set_property(TARGET papi PROPERTY IMPORTED_LOCATION ${PAPI_LIBRARY_PATH})
+set_property(TARGET papi APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${PAPI_INCLUDE_DIR})
+
+# Dependencies
+add_dependencies(papi papi_src)
